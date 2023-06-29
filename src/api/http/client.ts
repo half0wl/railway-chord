b18155a3a4a24a6bb8adbf0e291ddaf1e534fff4
Ray Chen <ray@raychen.io>
2023-06-29 02:31:33 +0200

    feat(*): Add better error handling

--- src/api/http/client.ts
+++ src/api/http/client.ts
@@ -1,16 +1,40 @@
 import { HttpClient } from '@/types'
-import { ApolloClient, InMemoryCache } from '@apollo/client/core'
+import {
+  ApolloClient,
+  ApolloLink,
+  HttpLink,
+  InMemoryCache,
+} from '@apollo/client/core'
+import { RetryLink } from '@apollo/client/link/retry'
 
 /**
  * Creates an authenticated GQL HTTP client.
  */
 const createHttpClient = (endpoint: string, apiToken: string): HttpClient => {
-  return new ApolloClient({
+  const httpLink = new HttpLink({
     uri: endpoint,
-    cache: new InMemoryCache(),
     headers: {
       Authorization: `Bearer ${apiToken}`,
     },
+  })
+
+  const retryLink = new RetryLink({
+    delay: {
+      initial: 300,
+      max: Infinity,
+      jitter: true,
+    },
+    attempts: {
+      max: 5,
+      retryIf: (error, _operation) => !!error,
+    },
+  })
+
+  const link = ApolloLink.from([retryLink, httpLink])
+
+  return new ApolloClient({
+    cache: new InMemoryCache(),
+    link,
   })
 }
 
